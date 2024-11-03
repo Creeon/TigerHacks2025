db6168d3be5501d502a7fffefedf393350602860
Creeon <creeon99@gmail.com>
2024-11-03 08:57:46 -0600

    Small main changes

--- main.py
+++ main.py
@@ -741,7 +741,7 @@ def toolsShop():
                            text_input = "BACK", font = get_font(20), base_color = (255, 255, 255), hovering_color = (0, 0, 0))
         
         tilling2_button = Button(image = pygame.transform.scale(pygame.image.load("images/MenuSprites/button2.png"), (190, 100)), pos = ((screen_width // 2) - 120, (screen_height // 2) - 200),
-                           text_input = "HOE (250)", font = get_font(20), base_color = (255, 255, 255), hovering_color = (0, 0, 0))
+                           text_input = "FORK (250)", font = get_font(20), base_color = (255, 255, 255), hovering_color = (0, 0, 0))
         tilling3_button = Button(image = pygame.transform.scale(pygame.image.load("images/MenuSprites/button2.png"), (190, 100)), pos = ((screen_width // 2) + 120, (screen_height // 2) - 200),
                            text_input = "TRACTOR (500)", font = get_font(20), base_color = (255, 255, 255), hovering_color = (0, 0, 0))
         
@@ -761,7 +761,7 @@ def toolsShop():
                            text_input = "MACHINE (500)", font = get_font(20), base_color = (255, 255, 255), hovering_color = (0, 0, 0))
         
         harvesting2_button = Button(image = pygame.transform.scale(pygame.image.load("images/MenuSprites/button2.png"), (190, 100)), pos = ((screen_width // 2) - 120, (screen_height // 2) + 200),
-                           text_input = "FORK (250)", font = get_font(20), base_color = (255, 255, 255), hovering_color = (0, 0, 0))
+                           text_input = "SCYTHE (250)", font = get_font(20), base_color = (255, 255, 255), hovering_color = (0, 0, 0))
         harvesting3_button = Button(image = pygame.transform.scale(pygame.image.load("images/MenuSprites/button2.png"), (190, 100)), pos = ((screen_width // 2) + 120, (screen_height // 2) + 200),
                            text_input = "COMBINE (500)", font = get_font(20), base_color = (255, 255, 255), hovering_color = (0, 0, 0))
         
@@ -905,7 +905,7 @@ day_writing = day_font.render("Day " + str(day), True, (0,0,0))
 day_rect = day_writing.get_rect(center=(screen_width-100, 50))
 
 money = Money()
-money.money=10000
+money.money=100
 calendar = Calendar()
 
 christmas_tiger = ChristmasTiger(800,400)
